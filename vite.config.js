fd55283a1471141ff610f17640ce6e56a3abfa1c
Guilherme Lana <127361301+coderlan4z@users.noreply.github.com>
2024-06-20 10:23:12 -0300

    Atualizar o vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/",
-  server: {
-    host: '0.0.0.0', 
-    port: 3000, 
-  },
-})
+  base: '/Deploy_BarberBook/', 
+  port: 3000
+});
